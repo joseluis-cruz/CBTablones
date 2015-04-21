4698bea25864a51fdbc3f7038b57abcb49308ceb
Pedro Saez <pedrosb29@gmail.com>
2015-04-21 20:54:23 +0100

    Commit Revisado
    Clase contacto revisada.

--- src/client/CBTablones/CBTablones/Resources/esquema/Contacto.cs
+++ src/client/CBTablones/CBTablones/Resources/esquema/Contacto.cs
@@ -8,26 +8,27 @@ namespace CBTablones
 	{
 		#region Constructor
 		public Contacto ()
-		{
-			
+		{			
 		}
 		#endregion
 
 		#region Propiedades
 
-		[PrimaryKey, AutoIncrementAttribute]
-		public int IDContacto{ get; set; }
+		[PrimaryKey, AutoIncrement]
+		public int ID{ get; set; }
 
-		[MaxLength(60)]
+		[MaxLength(100)]
 		public string EMail{ get; set; }
 
-		[MaxLength(60)]
+		[MaxLength(100)]
 		public string Nombre{ get; set; }
 
-		[MaxLength(60)]
+		[MaxLength(50),Unique]
 		public string Alias{ get; set; }
 
-		public DateTime DefaultVolat{ get; set; } 
+		public DateTime Volatilidad{ get; set; } 
+
+		public DateTime Caducidad{ get; set; } 
 
 		[MaxLength(1)]
 		public string Estado{ get; set; }
