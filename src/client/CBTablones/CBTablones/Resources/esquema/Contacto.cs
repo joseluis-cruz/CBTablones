27749c97de009a2ac1c3689468751f18d787d7f0
Pedro Saez <pedrosb29@gmail.com>
2015-04-21 20:36:33 +0100

    Agregada clase ORM para los contactos.

---
+++ src/client/CBTablones/CBTablones/Resources/esquema/Contacto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using SQLite;
+
+namespace CBTablones
+{
+	[Table("CONTACTOS")]
+	public class Contacto
+	{
+		#region Constructor
+		public Contacto ()
+		{
+			
+		}
+		#endregion
+
+		#region Propiedades
+
+		[PrimaryKey, AutoIncrementAttribute]
+		public int IDContacto{ get; set; }
+
+		[MaxLength(60)]
+		public string EMail{ get; set; }
+
+		[MaxLength(60)]
+		public string Nombre{ get; set; }
+
+		[MaxLength(60)]
+		public string Alias{ get; set; }
+
+		public DateTime DefaultVolat{ get; set; } 
+
+		[MaxLength(1)]
+		public string Estado{ get; set; }
+
+		#endregion
+	}
+}
+
