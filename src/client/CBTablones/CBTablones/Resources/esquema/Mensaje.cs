0f6042bc98d36bfc3534a3ffba8dfd64d5812196
cahs89 <cahs89@gmail.com>
2015-04-21 21:03:54 +0200

    Agregada Clase ORM para los Mensajes

---
+++ src/client/CBTablones/CBTablones/Resources/esquema/Mensaje.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using SQLite;
+
+namespace CBTablones
+{
+	[Table("MENSAJES")]
+	public class Mensaje
+	{
+
+		[PrimaryKey, AutoIncrement]
+		public int Id { get; set; }
+
+		public string Contenido { get; set; }
+
+		public int ContacOrigen { get; set; }
+
+		public int ContacDest { get; set; }
+
+		[AutoIncrement]
+		public int IdTablon { get; set; }
+
+		public DateTime FHoraCreacion { get; set; }
+
+		public DateTime FHoraServidor { get; set; }
+
+		public DateTime FHoraRecibe { get; set; }
+
+		public DateTime FHoraLee { get; set; }
+
+
+		public Mensaje ()
+		{
+
+		}
+	}
+}
+
