8049e5ef59f8efac87ba4458bf1fd87f67656af3
dexter1986 <jzamorano15@hotmail.com>
2015-05-13 03:23:41 +0200

    Agregada clase Tablones y mas
    Agregada la clase, adapter, Tab.
    Faltan cosas por terminar

---
+++ src/client/CBTablones/CBTablones/CrearTablon.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace CBTablones
+{
+	[Activity (Label = "Nuevo Tablon")]			
+	public class CrearTablon : Activity
+	{
+		private Button _BtnSiguiente;
+		private String[] _ListaCaducidades = {"1 Hora","1 Dia","1 Mes","Nunca"};
+		private String[] _ListaVolatilidades={"5 Segundos","20 Segundos","1 Minuto","1 Hora","Nunca"};
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Create your application here
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.CrearTablon);
+
+			_BtnSiguiente = FindViewById<Button> (Resource.Id.btnCrearTablon);
+
+			_BtnSiguiente.Click += delegate {AbrirVentana (typeof(AddContactsTablon));};
+
+
+
+		}
+
+		private void AbrirVentana(Type activityType)
+		{
+			var myIntent = new Intent (Application.Context, activityType);
+			StartActivity (myIntent);
+		}
+	}
+}
+
---
+++ src/client/CBTablones/CBTablones/Resources/adapter/TablonAdapter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Android.Widget;
+using Android.App;
+using Android.Views;
+
+namespace CBTablones
+{
+	public class TablonAdapter:BaseAdapter<Tablon>
+	{
+		List <Tablon> items;
+		Activity context;
+
+		public TablonAdapter (Activity context, List<Tablon> items)
+		{
+			this.context = context;
+			this.items = items;
+		}
+
+		#region implemented abstract members of BaseAdapter
+
+		public override long GetItemId (int position)
+		{
+			return position;
+		}
+
+		public override Android.Views.View GetView (int position, Android.Views.View convertView, Android.Views.ViewGroup parent)
+		{
+			var item = items [position];
+
+			View view = convertView;
+			if (view == null)
+				view = context.LayoutInflater.Inflate (Resource.Layout.ListaTablonesItemLo, null);
+			view.FindViewById<TextView> (Resource.Id.tvNombre).Text = item.Nombre.ToString ();
+
+			return view;
+		}
+
+		public override int Count {
+			get {
+				return items.Count;
+			}
+		}
+
+		#endregion
+
+		#region implemented abstract members of BaseAdapter
+
+		public override Tablon this [int index] {
+			get {
+				return items[index];
+			}
+		}
+
+		#endregion
+	}
+}
+
